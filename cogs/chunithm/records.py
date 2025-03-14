952c9939a834a970451ed773cf6b4ba693547160
beer-psi <lacvtg.a1.2023@gmail.com>
2025-03-14 17:28:56 +0700

    fix(r10): fix number of rating slots

--- cogs/chunithm/records.py
+++ cogs/chunithm/records.py
@@ -1202,7 +1202,7 @@ class RecordsCog(commands.Cog, name="Records"):
             recent10 = await client.recent10()
             recent10 = await self.utils.hydrate_records(recent10)
 
-            view = B30View(ctx, recent10, show_reachable=False)
+            view = B30View(ctx, recent10, rating_slots=10, show_reachable=False)
             view.message = await ctx.reply(
                 content=view.format_content(),
                 embeds=view.format_page(view.items[: view.per_page]),
