c1de8fca5c7f03e1f201752c88cf05f8a826e91f
beer-psi <lacvtg.a1.2023@gmail.com>
2025-06-23 19:44:20 +0700

    don't blast ears off episode 3

--- cogs/gaming/_session.py
+++ cogs/gaming/_session.py
@@ -284,7 +284,7 @@ class GuessingGameSession:
             "-acodec",
             "libopus",
             "-filter:a",
-            "volume=0.20",
+            "volume=0.15",
             "pipe:1",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.DEVNULL,
