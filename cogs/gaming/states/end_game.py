941b7adaf5d7f0006eb0e43db4cf3dcea553395c
beer-psi <lacvtg.a1.2023@gmail.com>
2025-06-25 20:19:13 +0700

    another end game tip

--- cogs/gaming/states/end_game.py
+++ cogs/gaming/states/end_game.py
@@ -37,6 +37,7 @@ STATIC_TIPS = tips = [
     "put the maid dress on",
     "you could be playing balatro right now",
     "Play UNREAL LIFE",
+    "c>guess is actually the shittiest fucking minigame. Ugly ass UI, bot keeps rebooting, crops that are honestly impossible, the only people enjoying the game are 30 somethings who have played the game for 15 years",
     "go ahead, look up the source for these tips, ruin the fun for yourself",
 ]
 
