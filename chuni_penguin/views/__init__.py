1da1d014f5fb6365f6e89e28e88da7961f18fe10
beer-psi <lacvtg.a1.2023@gmail.com>
2025-10-05 21:12:57 +0700

    re-export views

--- chuni_penguin/views/__init__.py
+++ chuni_penguin/views/__init__.py
@@ -1,9 +1,32 @@
-from chuni_penguin.views.b30 import B30View
-from chuni_penguin.views.b30n20 import B30N20View
-from chuni_penguin.views.courses import CourseListView
-from chuni_penguin.views.embeds import EmbedPaginationView
-from chuni_penguin.views.login import LoginFlowView
-from chuni_penguin.views.profile import ProfileView
-from chuni_penguin.views.recent import RecentRecordsView
-from chuni_penguin.views.select_to_compare import SelectToCompareView
-from chuni_penguin.views.songlist import SonglistView
+from .b30 import B30View
+from .b30n20 import B30N20View
+from .confirmation import ConfirmationYesView
+from .courses import CourseListView
+from .embeds import EmbedPaginationView
+from .gaming import GuessLeaderboardView, RetryGameButton
+from .leaderboard import LeaderboardView
+from .login import LoginFlowView
+from .login_bonus import LoginBonusView
+from .profile import ProfileView
+from .recent import RecentRecordsView
+from .select_to_compare import SelectToCompareView
+from .song_info import SongInfoPaginationView
+from .songlist import SonglistView
+
+__all__ = (
+    "B30N20View",
+    "B30View",
+    "ConfirmationYesView",
+    "CourseListView",
+    "EmbedPaginationView",
+    "GuessLeaderboardView",
+    "LeaderboardView",
+    "LoginBonusView",
+    "LoginFlowView",
+    "ProfileView",
+    "RecentRecordsView",
+    "RetryGameButton",
+    "SelectToCompareView",
+    "SongInfoPaginationView",
+    "SonglistView",
+)
