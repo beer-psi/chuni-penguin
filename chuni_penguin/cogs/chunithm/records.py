dfe6744e6e93d28a554bb3c45e2d0c28d30bb954
beer-psi <lacvtg.a1.2023@gmail.com>
2025-10-17 14:27:39 +0700

    fix(records/compare): use MessageReference.resolved when available

--- chuni_penguin/cogs/chunithm/records.py
+++ chuni_penguin/cogs/chunithm/records.py
@@ -802,13 +802,18 @@ class RecordsCog(commands.Cog, name="Records"):
         ):
             message: discord.Message | discord.MessageSnapshot
 
-            if (
-                ctx.message.reference is not None
-                and ctx.message.reference.message_id is not None
-            ):
-                message = await ctx.channel.fetch_message(
-                    ctx.message.reference.message_id
-                )
+            if (reference := ctx.message.reference) is not None:
+                if isinstance(reference.resolved, discord.Message):
+                    message = reference.resolved
+                elif reference.message_id is not None:
+                    try:
+                        message = await ctx.channel.fetch_message(reference.message_id)
+                    except discord.HTTPException:
+                        msg = "Could not fetch the message that was replied to. Is it deleted?"
+                        raise commands.CommandError(msg) from None
+                else:
+                    msg = "The message reference did not point to a valid message."
+                    raise commands.BadArgument(msg)
             else:
                 try:
 
