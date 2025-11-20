8a307b5db63e3059de969f7569347cf839863308
beer-psi <lacvtg.a1.2023@gmail.com>
2025-11-20 09:37:01 +0700

    switch back to verse for kt

--- chuni_penguin/constants.py
+++ chuni_penguin/constants.py
@@ -11,7 +11,7 @@ MAX_DIFFICULTY = 15.7
 
 # Used to split old records from new records.
 CURRENT_CHUNITHM_VERSION = "X-VERSE"
-CURRENT_CHUNITHM_VERSION_KT = f"CHUNITHM {CURRENT_CHUNITHM_VERSION}"
+CURRENT_CHUNITHM_VERSION_KT = "CHUNITHM VERSE"
 
 # The version names are just my favorite CHUNITHM songs
 # in no particular order.
