c4e476b0583ad583cc7e71c1074326232e3e7b0e
beer-psi <lacvtg.a1.2023@gmail.com>
2026-01-13 09:14:55 +0700

    also fix default in guess_leaderboard

--- chuni_penguin/database/alembic/versions/2026_01_13_0853-304e2e43729c_fix_1_in_uint64.py
+++ chuni_penguin/database/alembic/versions/2026_01_13_0853-304e2e43729c_fix_1_in_uint64.py
@@ -8,6 +8,7 @@ Create Date: 2026-01-13 08:53:55.846533
 
 from typing import Sequence, Union
 
+import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
@@ -18,10 +19,16 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
+    with op.batch_alter_table("guess_leaderboard") as batch_op:
+        batch_op.alter_column("guild_id", server_default=sa.text("0"))
+
     op.execute("UPDATE aliases SET guild_id = 0 WHERE guild_id = -1")
     op.execute("UPDATE guess_leaderboard SET guild_id = 0 WHERE guild_id = -1")
 
 
 def downgrade() -> None:
+    with op.batch_alter_table("guess_leaderboard") as batch_op:
+        batch_op.alter_column("guild_id", server_default=sa.text("-1"))
+
     op.execute("UPDATE aliases SET guild_id = -1 WHERE guild_id = 0")
     op.execute("UPDATE guess_leaderboard SET guild_id = -1 WHERE guild_id = 0")
