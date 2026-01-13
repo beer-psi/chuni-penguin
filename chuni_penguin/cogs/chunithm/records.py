7d65a9189104c87469494c0b80e7883a647df277
beer-psi <lacvtg.a1.2023@gmail.com>
2026-01-13 23:57:28 +0700

    chore: lint

--- chuni_penguin/cogs/chunithm/records.py
+++ chuni_penguin/cogs/chunithm/records.py
@@ -15,7 +15,7 @@ import discord
 from discord import Interaction, app_commands
 from discord.ext import commands
 from discord.utils import escape_markdown
-from sqlalchemy import func, select
+from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 
 from chuni_penguin import flags
