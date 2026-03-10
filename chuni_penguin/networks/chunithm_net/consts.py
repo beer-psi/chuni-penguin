c47f876fc1f14b1ebc122b7019fe88341b675cd2
beer-psi <92439990+beer-psi@users.noreply.github.com>
2026-03-10 09:17:00 +0700

    Add image names for Linked GATE CRYSTAL badges

--- chuni_penguin/networks/chunithm_net/consts.py
+++ chuni_penguin/networks/chunithm_net/consts.py
@@ -18,10 +18,12 @@ LINKED_VERSE_PROGRESS_BADGES: dict[str, tuple[LinkedGate | None, LinkedGateStatu
     "43CM9OC9J34RXAQ4N24PFS7ZTI1W4X8Z": (LinkedGate.air, LinkedGateStatus.linkable),
     "TU4SRWXCY1OKDO4R0HW8CLXR4UGWOALL": (LinkedGate.air, LinkedGateStatus.clear),
     "A1JQ45K97ZIB50JONO302LDV9OKZ96KC": (LinkedGate.star, LinkedGateStatus.not_found),
-    # "": (LinkedGate.star, LinkedGateStatus.under_analysis),
+    "A0YQIQFHIYWS39NTVBUT4MAC2SZ9OGNV": (
+        LinkedGate.star,
+        LinkedGateStatus.under_analysis,
+    ),
     "K358WG6QFX2Q5CVVFCSUXKR42TQLOOGM": (LinkedGate.star, LinkedGateStatus.linkable),
     "VRMAQKZAFXWXBH9LK768FDYFV2AJUA7A": (LinkedGate.star, LinkedGateStatus.clear),
-    # They reuse the same not_found badge for CRYSTAL for some reason?
     "IB6KI2OFKMIJ2KPCH3JWS8OKJQ69J5QQ": (LinkedGate.amazon, LinkedGateStatus.not_found),
     "2VUVKET50AOXQD0RAQ3SMFBVP7YTPGOO": (
         LinkedGate.amazon,
@@ -30,9 +32,16 @@ LINKED_VERSE_PROGRESS_BADGES: dict[str, tuple[LinkedGate | None, LinkedGateStatu
     "O2R15SSPWS2JYGF1U6GDUMW3UW7K0A11": (LinkedGate.amazon, LinkedGateStatus.linkable),
     "FTDY9GBIZERC5J9VZN882WFMKFLCIEZ7": (LinkedGate.amazon, LinkedGateStatus.clear),
     "YRI8UKP26A4F4G810VAYZW9T9SXO49BP": (
-        LinkedGate.paradise,
+        LinkedGate.crystal,
         LinkedGateStatus.not_found,
     ),
+    "9Q6I0MKPUYRL3MMG191BR5I45JREKO7M": (
+        LinkedGate.crystal,
+        LinkedGateStatus.under_analysis,
+    ),
+    "P09LZYXJHAG7F78TE43DGH826GAD2H2C": (LinkedGate.crystal, LinkedGateStatus.linkable),
+    "TEFG9MDNMX2X5IYPRYJYCUDNC8X3E49U": (LinkedGate.crystal, LinkedGateStatus.clear),
+    # They reuse the same not_found badge from CRYSTAL for PARADISE for some reason?
     "TCMI6URY6CA2DZGOPNQ13KRE1A8V3UC8": (LinkedGate.new, LinkedGateStatus.not_found),
     "QF07NCIW9AQKZOUF2OXP0MWLWG1BLBCM": (LinkedGate.sun, LinkedGateStatus.not_found),
     "7FFQEALW1J8TXTOG2ONB6PHPNA2252Q2": (
